e9db7a93ec05901081781bbcd12176bc0719eea0
Strider-26 <Mohanadkhaled87@gmail.com>
2023-01-29 23:25:35 +0200

    first

--- Source.cpp
+++ Source.cpp
@@ -4,11 +4,11 @@ int main()
 {
     RenderWindow window(VideoMode::getFullscreenModes()[0], "SFML works!", Style::None);
 
-    RectangleShape rectangle;
+    /*RectangleShape rectangle;
     rectangle.setSize(sf::Vector2f(100, 50));
     rectangle.setOutlineThickness(5);
     rectangle.setPosition(10, 20);
-    rectangle.setFillColor(Color::Green);
+    rectangle.setFillColor(Color::Green);*/
 
     while (window.isOpen())
     {
@@ -17,7 +17,7 @@ int main()
         {
             if (event.type == Event::Closed || Keyboard::isKeyPressed(Keyboard::Escape))
                 window.close();
-            if (rectangle.getPosition().x == window.getSize().x || rectangle.getPosition().y == window.getSize().y)
+            /*if (rectangle.getPosition().x == window.getSize().x || rectangle.getPosition().y == window.getSize().y)
                 rectangle.setPosition(0, 0);
             if (Keyboard::isKeyPressed(Keyboard::Right))
                 rectangle.setPosition(Vector2f(5 + rectangle.getPosition().x, rectangle.getPosition().y));
@@ -26,11 +26,11 @@ int main()
             if (Keyboard::isKeyPressed(Keyboard::Up))
                 rectangle.setPosition(Vector2f(rectangle.getPosition().x, rectangle.getPosition().y - 5));
             if (Keyboard::isKeyPressed(Keyboard::Down))
-                rectangle.setPosition(Vector2f(rectangle.getPosition().x, 5 + rectangle.getPosition().y));
+                rectangle.setPosition(Vector2f(rectangle.getPosition().x, 5 + rectangle.getPosition().y));*/
         }
 
         window.clear();
-        window.draw(rectangle);
+        //window.draw(rectangle);
         window.display();
     }
 
